178afde1ce2b8d82b599343cbdc67a8232e5f30a
Malik Cherfi <malikcherfi@gmail.com>
2022-10-25 17:40:59 +0200

    add login form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,46 @@
 import Container from "../../components/styled-components/Container";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  return <Container>login</Container>;
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Adresse mail :
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+        </label>
+        <label>
+          Mot de passe :
+          <input
+            type="text"
+            name="password"
+            placeholder="Mot de passe"
+            {...register("password", { required: true })}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <div>
+        <p>Vous n'etes pas inscrit ?</p>
+        <p>
+          <Link
+            to="/register"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Créer un compte
+          </Link>
+        </p>
+      </div>
+    </Container>
+  );
 };
 
 export default Login;
