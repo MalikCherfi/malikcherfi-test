a1cc5ac83f44b47eaae24b4932bada912b4eded3
Malik Cherfi <malikcherfi@gmail.com>
2022-10-25 22:17:34 +0200

    add cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,22 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import { setupRoutes } from "./routes/index.js";
 const app = express();
 
 const port = process.env.PORT || 8000;
 
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: true, limit: "25mb" }));
+
+app.use(express.static("public"));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+setupRoutes(app);
 
 app.listen(port, () => {
   console.log("Server app listening on port " + port);
