cea7179eea841a5de493ccb390c46a023abc6e9d
Malik Cherfi <malikcherfi@gmail.com>
2022-10-26 20:38:57 +0200

    add style for h1 and h2

---
+++ src/components/styled-components/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+ h1 {
+    font-family: 'Rancho', cursive;
+ }
+
+ h2 {
+   font-family: 'Rancho', cursive;
+ }
+`;
+
+export default GlobalStyle;
