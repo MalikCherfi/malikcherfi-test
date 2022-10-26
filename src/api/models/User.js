aea86416a75eff0c19d1199910fcde20d7dd6e40
Malik Cherfi <malikcherfi@gmail.com>
2022-10-26 15:04:59 +0200

    add name and lastname to user model

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
+  name: String,
+  lastName: String,
   email: String,
   password: String,
 });
