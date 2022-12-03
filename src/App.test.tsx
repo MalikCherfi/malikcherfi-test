62c08779f46ff6196073931f8c04794bd515493b
Malik Cherfi <malikcherfi@gmail.com>
2022-12-03 17:25:04 +0100

    change app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("render App", () => {
+  it("renders learn react link", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const linkElement = screen.getByText(/learn react/i);
+    // Assert
+    expect(linkElement).toBeInTheDocument();
+  });
 });
